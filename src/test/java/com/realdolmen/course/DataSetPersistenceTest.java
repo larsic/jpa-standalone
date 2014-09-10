e4bca0e0cbd1c742c619a896d9ae3e4b39e4d34b
Kevin Van Robbroeck <kevin.vanrobbroeck@gmail.com>
2014-09-10 10:07:19 +0200

    database config in datasetPersistenceTest (cherry picked from commit bed1137)

--- src/test/java/com/realdolmen/course/DataSetPersistenceTest.java
+++ src/test/java/com/realdolmen/course/DataSetPersistenceTest.java
@@ -1,10 +1,12 @@
 package com.realdolmen.course;
 
+import org.dbunit.database.DatabaseConfig;
 import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.ext.mysql.MySqlDataTypeFactory;
 import org.dbunit.operation.DatabaseOperation;
 import org.junit.Before;
 import org.slf4j.Logger;
@@ -24,6 +26,11 @@ public class DataSetPersistenceTest extends PersistenceTest {
                 "jdbc:mysql://localhost:3306/test", "root", "root");
         IDatabaseConnection connection = new DatabaseConnection(jdbcConnection);
         IDataSet dataSet = new FlatXmlDataSetBuilder().build(getClass().getResource("/data.xml"));
+
+        //set factorytype in dbconfig to remove warning
+        DatabaseConfig dbConfig = connection.getConfig();
+        dbConfig.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new MySqlDataTypeFactory());
+
         DatabaseOperation.CLEAN_INSERT.execute(connection, dataSet);
     }
 }
